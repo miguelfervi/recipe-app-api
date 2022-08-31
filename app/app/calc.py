9687809fe7896153d79693622f862ebb33b2e91d
Miguel Fernandez <miguelfervi@gmail.com>
2022-08-31 15:59:53 +0200

    add some test

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """Add x and y and retur result."""
+    return x+y
+
+def substract(x,y):
+    """Substract x and y and return results"""
+    return y - x 
